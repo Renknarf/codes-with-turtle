20d50eda168d26a49c5bdc49d847749bd4cb3a08
Frankner S. Soares <58006101+Renknarf@users.noreply.github.com>
2024-03-19 12:27:31 -0400

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,10 @@
+# Python program to draw square
+# using Turtle Programming
+import turtle
+skk = turtle.Turtle()
+ 
+for i in range(4):
+    skk.forward(100)
+    skk.left(90)
+     
+turtle.done()
---
+++ TurtleExample.py
@@ -0,0 +1,12 @@
+import turtle
+
+s = turtle.getscreen()
+
+t = turtle.Turtle()
+
+t.right(45)
+t.forward(50)
+t.left(150)
+t.backward(200)
+
+t.goto(200,200)
